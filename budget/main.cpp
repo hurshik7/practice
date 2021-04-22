a885b3f2964b32da7b3ce62682f8d7759f532281
hurshik <>
2021-04-22 14:53:48 +0900

    100%

--- budget/main.cpp
+++ budget/main.cpp
@@ -2,24 +2,42 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> d, int budget) 
 {
     int answer = 0;
-    int dsize = d.size();
-    
-    for (int i = dsize; i > 0; i--) {
-        :
-    }     
-    
-  
+    vector<int>::size_type dsize = d.size();
+    sort(d.begin(), d.end());
+    int i;    
+
+    int max_count = 0;
+    answer = dsize;
+    int temp;
+    while (answer > 0) {
+        temp = 0;
+        for (i = 0; i < answer; i++) {
+            temp += d[i];
+        }
+
+        if (temp <= budget) {
+            if (max_count < answer) {
+                max_count = answer;
+            } else {
+                break;
+            }
+        }
+        answer--;
+    }    
+    answer = max_count;
     return answer;
 }
 
 int main(void)
 {
-
+    cout << solution({1, 3, 2, 5, 4}, 9) << endl;
+    cout << solution({2, 2, 3, 3}, 10) << endl;
     return 0;
 }
