10eb2a034d6328ff5dde78fd6b8b7338f11515a1
hurshik7 <hurshik7@gmail.com>
2022-01-09 15:43:24 -0800

    solved 48.6% / 62.73%

--- leetcode/merge_sorted_array/merge_sorted_array.py
+++ leetcode/merge_sorted_array/merge_sorted_array.py
@@ -1,6 +1,13 @@
 
 def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    index2 = 0
+    for i in range(0, len(nums1)):
+        if nums1[i] == 0:
+            if index2 <= n:
+                nums1[i] = nums2[index2]
+                index2 += 1
 
+    nums1.sort()
 
 
 nums1 = [1, 2, 3, 0, 0, 0]
