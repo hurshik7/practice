d297ab14fa4f0f8af2156610c46212fdf91899c9
hurshik7 <hurshik7@gmail.com>
2022-01-09 17:55:51 -0800

    in-progress

---
+++ leetcode/symmetric_tree/symmetric_tree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../TreeNode.cpp"
+
+using namespace std;
+
+bool isSymmetric(TreeNode* root) 
+{
+    if (root == nullptr) {
+        return true;
+    }  
+
+    
+}
+
+int main(void)
+{
+        
+    return 0;
+}
+
+
