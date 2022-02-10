34a3817c535a0f9b9c3562d16e4e0830ba616631
hurshik7 <hurshik7@gmail.com>
2022-02-10 11:20:29 -0800

    solved

---
+++ leetcode/pow/pow.c
@@ -0,0 +1,23 @@
+
+
+double myPow(double x, int n){
+    if (n == 0) {
+        return 1;
+    }
+    if (n == 1) {
+        return x;
+    }
+    if (n % 2 == 0) {
+        if (n < 0) {
+            if (n == INT_MIN) {
+                n += 1;
+            }
+            return 1 / (myPow(x * x, (-n) / 2));
+        }
+        return myPow(x * x, n / 2);
+    }
+    if (n < 0) {
+        return 1 / (x * myPow(x, -n - 1));
+    }
+    return x * myPow(x, n - 1);
+}
