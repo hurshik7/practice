0cd75a214f5caa614092f560fff84ea05a1ccdf1
hurshik7 <hurshik7@gmail.com>
2022-04-27 17:58:58 -0700

    solved, 98% / 65.73%

---
+++ leetcode/missing_number/missing_number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        int sum2 = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            sum2 += i + 1;
+        }
+        return sum2 - sum;
+    }
+};
